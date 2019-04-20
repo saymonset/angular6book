9e28d165d39a2e611950f6ff28dfdb19695b614f
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-20 13:10:20 -0400

    Colocando classes dinamicamente en la vista, dependiendo que prducto es con built-in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { ApplicationRef,Component } from '@angular/core';
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
 @Component({
   selector: 'app',
   templateUrl: './template.html',
@@ -7,4 +8,31 @@ import { Model } from "./repository.model";
 })
 export class AppComponent {
   model: Model = new Model();
+
+  constructor(ref: ApplicationRef) {
+		(<any>window).appRef = ref;
+		(<any>window).model = this.model;
+	}
+
+	getClasses(key: number): string {
+		let product = this.model.getProduct(key);
+		return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+	}
+
+	getClassMap(key: number): Object {
+		let product = this.model.getProduct(key);
+			return {
+					"text-center bg-danger": product.name == "Kayak",
+					"bg-info": product.price < 50
+			};
+		}
+
+		getProductByPosition(position: number): Product {
+			return this.model.getProducts()[position];
+		}
+		
+		getClassesByPosition(position: number): string {
+			let product = this.getProductByPosition(position);
+			return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+		}
 }

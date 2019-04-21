74eb415cde0a0960892dfdfe13b0490e837b5dd5
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-21 11:21:17 -0400

    Form Validation complete. Chapter 14.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { ApplicationRef,Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
+import { ProductFormGroup } from "./form.model";
+import { LimitValidator } from "./limit.formvalidator";
 @Component({
   selector: 'app',
   templateUrl: './template.html',
@@ -8,6 +11,7 @@ import { Product } from "./product.model";
 })
 export class AppComponent {
   model: Model = new Model();
+  form: ProductFormGroup = new ProductFormGroup();
 
  
 
@@ -20,9 +24,64 @@ export class AppComponent {
 			return this.model.getProducts();
 			}
 		
-		selectedProduct: string;
-		
-		getSelected(product: Product): boolean {
-			return product.name == this.selectedProduct;
+		newProduct: Product = new Product();
+
+		get jsonProduct() {
+			return JSON.stringify(this.newProduct);
 		}
+
+		addProduct(p: Product) {
+			console.log("New Product: " + this.jsonProduct);
+		}
+
+
+		getValidationMessages(state: any, thingName?: string) {
+			let thing: string = state.path || thingName;
+			let messages: string[] = [];
+			if (state.errors) {
+				for (let errorName in state.errors) {
+					switch (errorName) {
+						case "required":
+							messages.push(`You must enter a ${thing}`);
+							break;
+						case "minlength":
+							messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+							break;
+
+					
+						case "pattern":
+							messages.push(`The ${thing} contains illegal characters`);
+							break;
+						}
+					}
+				}
+				return messages;
+			}
+
+
+			formSubmitted: boolean = false;
+
+			submitForm(form: NgForm) {
+				this.formSubmitted = true;
+				if (form.valid) {
+					this.addProduct(this.newProduct);
+					this.newProduct = new Product();
+					form.reset();
+					this.formSubmitted = false;
+				}
+			}
+
+                 
+
+
+                	getFormValidationMessages(form: NgForm): string[] {
+					let messages: string[] = [];
+						Object.keys(form.controls).forEach(k => {
+							this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
+						});
+						return messages;
+					}
+
+  
+
 }
